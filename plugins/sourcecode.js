75d00c4bfa4cb5d78e305d9f0b1aecd6c5acfda3
Dimzga <106530321+Dimzga@users.noreply.github.com>
2022-06-16 21:30:05 +0700

    Add files via upload

---
+++ plugins/sourcecode.js
@@ -0,0 +1,26 @@
+let handler = async (m, { conn }) => {
+	
+	// Link Script jgn di ganti ya pliss
+    // Kalo mau masukin github kamu tinggal add aja link githubnya tapi jangan di hapus sumber script!
+    // Jangan Ngeyell..
+    // Kalo ngeyel gk bakal gw up sc lgi.
+    
+    let txt = `
+Bot ini menggunakan script github
+
+https://github.com/FokusDotId/Family-MD.git
+
+pliss follow dan kasih ✨⭐⭐🌟 ya pak!
+
+My Github: https://github.com/FokusDotId
+
+`
+     conn.reply(m.chat, txt, m)
+}
+handler.help = ['sourcecode']
+handler.tags = ['info']
+handler.command = /^(sc(ript(bot)?)?|sourcecode)$/i
+
+module.exports = handler
+
+
