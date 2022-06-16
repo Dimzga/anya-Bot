35640750a41a1c01e68b01e6ec68213add01ae15
Dimzga <106530321+Dimzga@users.noreply.github.com>
2022-06-16 12:27:26 +0700

    Add files via upload

---
+++ plugins/sewabot.js
@@ -0,0 +1,54 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => {
+  let pepe = 'https://telegra.ph/file/2e2eb7d093fec08fd00d4.jpg'
+  let baper = await fetch(pepe).then(a => a.buffer())
+
+  let aine = '6883894556422@s.whatsapp.net'
+  let a = await conn.profilePictureUrl(conn.user.jid, 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
+  let b = await conn.profilePictureUrl(owner[0]+'@s.whatsapp.net', 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
+  let c = pickRandom([a, b])
+  let d = await fetch(c).then(a => a.buffer())
+  let prepare = await require('@adiwajshing/baileys').generateWAMessageFromContent(m.key.remoteJid,{listMessage:{
+  title: `${await conn.getName(conn.user.jid)}`,
+  description: ` *• SEWA BOT & UP TO Moderator •*
+        
+1. Grup / 30 Hari
+Rp. 15.000 Dana
+Rp. 15.000 Pulsa
+
+2. moderator / 30 Hari
+Rp. 20.000 Dana
+Rp. 20.000 Pulsa
+
+
+
+wa.me/${owner[0]}
+*Bukan Bot!!!*
+*Owner ${conn.user.name}*
+`,
+  buttonText: 'Harga Sesuai Pasaran',
+  listType: 2,
+  productListInfo: {
+  productSections: [{
+  title:'Klik untuk order',
+  products:[{productId:''}]}],
+  headerImage: { productId: '',
+  jpegThumbnail: baper },
+  businessOwnerJid: `6883894556422@s.whatsapp.net`
+  },
+  footerText: 'no spam',
+  }},{})
+  conn.relayMessage(prepare.key.remoteJid,prepare.message,{messageId:prepare.key.id})
+  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+  conn.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+
+}
+handler.help = ['sewa']
+handler.tags = ['main']
+handler.command = /^(sewa)$/i
+
+module.exports = handler
+
+function pickRandom(list) {
+        return list[Math.floor(Math.random() * list.length)]
+    }
