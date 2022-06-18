689d1f9550072ab79e38d6892c1d9548b4776579
Dimzga <106530321+Dimzga@users.noreply.github.com>
2022-06-18 16:04:21 +0700

    Add files via upload

---
+++ plugins/Daftar anime all.js
@@ -0,0 +1,83 @@
+let handler = async (m, { conn, command }) => {
+const bg = "https://telegra.ph/file/6c943fc155456e5cc30de.jpg"
+
+let krtu = `Kartu Intro`
+m.reply(`
+.akira
+
+.akiyama
+
+.anna
+
+.asuna
+
+.ayuzawa
+
+.boruto
+
+.chiho
+
+.chitoge
+
+.deidara
+
+. erza
+
+.elaina
+
+.eba
+
+.emilia
+
+.hestia
+
+.hinata
+
+.inori
+
+.isuzu
+
+.itachi
+
+.itori
+
+.kaga
+
+.kagura
+
+.kaori
+
+.keneki
+
+.kotori
+
+.kurumi
+
+.madara
+
+.mikasa
+
+.miku
+
+.minato
+
+.naruto
+
+.nezuko
+
+.sagiri
+
+.sasuke
+
+.sakura
+
+
+/konachan <query>
+
+`.trim()) // Tambah sendiri kalo mau
+}
+handler.command = /^(animeall)$/i
+handler.mods = false
+handler.owner = false
+
+module.exports = handler
