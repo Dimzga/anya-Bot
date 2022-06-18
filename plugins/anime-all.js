c7d5546836e34c7d68d25974d262b1b6212a183f
Dimzga <106530321+Dimzga@users.noreply.github.com>
2022-06-18 16:16:44 +0700

    Delete anime-all.js

--- plugins/anime-all.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn, command }) => {
-let nyenye = `https://api-reysekha.herokuapp.com/api/wallpaper/${command}?apikey=APIKEY`
-}
-handler.help = ['akira', 'akiyama', 'anna', 'asuna', 'ayuzawa', 'boruto', 'chiho', 'chitoge', 'deidara', 'erza', 'elaina', 'eba', 'emilia', 'hestia', 'hinata', 'inori', 'isuzu', 'itachi', 'itori', 'kaga', 'kagura', 'kaori', 'keneki', 'kotori', 'kurumi', 'madara', 'mikasa', 'miku', 'minato', 'naruto', 'nezuko', 'sagiri', 'sasuke', 'sakura', 'cosplay']
-handler.tags = ['anime']
-handler.command = /^(mikasa)$/i
-//buatan hyzer, jgn hapus 🗿
-module.exports = handler
