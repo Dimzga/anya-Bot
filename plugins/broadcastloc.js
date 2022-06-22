1f42b14e5b38dcedd08da37e32d0f4e8ba3fb8ca
Dimzga <106530321+Dimzga@users.noreply.github.com>
2022-06-22 09:48:38 +0700

    Update broadcastloc.js

--- plugins/broadcastloc.js
+++ plugins/broadcastloc.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text }) => {
     let groups = Object.keys(await conn.groupFetchAllParticipating())
     m.reply(`_mengirim pesan siaran ke ${groups.length} grup_\nestimasi selesai ${groups.length * 1.5} detik`)
     for (let id of groups) {
-        let bg = 'https://telegra.ph/file/16e56993b190abd3a2913.jpg'
+        let bg = 'https://telegra.ph/file/a5591e5c03ecbc0e50b05.jpg'
         await conn.delay(1500)
         await conn.sendButtonLoc(id, await (await fetch(bg)).buffer(), text, wm, 'broadcast', 'broadcast')
     }
