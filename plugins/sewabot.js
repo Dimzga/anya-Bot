36e2d673799ae0e4378192439a7235188843b6db
Dimzga <106530321+Dimzga@users.noreply.github.com>
2022-06-22 09:54:33 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -9,7 +9,7 @@ m.reply(`
 │•> .1. Grup / 30 Har*
 │•> .Rp. 15.000 Ovo
  | •> .Rp. 15.000 Pulsa
- | •> 2. moderator + MODERATOR 30 Hari
+ | •> 2. GROUP + MODERATOR 30 Hari
  | •>Rp. 20.000 Ovo
  | •>Rp. 20.000 Pulsa
  | •> https://wa.me/qr/7Y5SVWOXU7FZP1
