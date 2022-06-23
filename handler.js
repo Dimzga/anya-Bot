8fe06bd5ebfa82d6e85c4ce8930daebabee43b2c
Dimzga <106530321+Dimzga@users.noreply.github.com>
2022-06-23 15:10:36 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -720,3 +720,460 @@ function clockString(ms) {
     let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
     return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 }
+        }
+
+        let chat = global.db.data.chats[m.chat]
+        if (typeof chat !== 'object') global.db.data.chats[m.chat] = {}
+        if (chat) {
+          if (!('isBanned' in chat)) chat.isBanned = false
+          if (!('welcome' in chat)) chat.welcome = false
+          if (!('detect' in chat)) chat.detect = false
+          if (!('sWelcome' in chat)) chat.sWelcome = ''
+          if (!('sBye' in chat)) chat.sBye = ''
+          if (!('sPromote' in chat)) chat.sPromote = ''
+          if (!('sDemote' in chat)) chat.sDemote = ''
+          if (!('descUpdate' in chat)) chat.descUpdate = true
+          if (!('delete' in chat)) chat.delete = false
+          if (!('antiBadword' in chat)) chat.antiBadword = true
+          if (!('rpg' in chat)) chat.delete = true
+          if (!('nsfw' in chat)) chat.delete = true
+          if (!('antiLink' in chat)) chat.antiLink = false
+          if (!('viewonce' in chat)) chat.viewonce = true
+          if (!('simi' in chat)) chat.sPromote = false
+          if (!('nsfw' in chat)) chat.nsfw = true
+        } else global.db.data.chats[m.chat] = {
+          isBanned: false,
+          welcome: false,
+          detect: false,
+          sWelcome: '',
+          sBye: '',
+          sPromote: '',
+          sDemote: '',
+          descUpdate: true,
+          delete: false,
+          rpg: true,
+          nsfw: true,
+          antiBadword: true,
+          antiLink: false,
+          viewonce: true,
+          simi: false,
+        }
+        
+                let settings = global.db.data.settings[this.user.jid]
+        if (typeof settings !== 'object') global.db.data.settings[this.user.jid] = {}
+        if (settings) {
+          if (!'anon' in settings) settings.anon = true
+          if (!'anticall' in settings) settings.anticall = true
+          if (!'antispam' in settings) settings.antispam = true
+          if (!'antitroli' in settings) settings.antitroli = true
+          if (!'backup' in settings) settings.backup = true
+          if (!isNumber(settings.backupDB)) settings.backupDB = 0
+          if (!'groupOnly' in settings) settings.groupOnly = false
+          if (!'jadibot' in settings) settings.groupOnly = false
+          if (!'nsfw' in settings) settings.nsfw = true
+          if (!isNumber(settings.status)) settings.status = 0
+        } else global.db.data.settings[this.user.jid] = {
+          anon: true,
+          anticall: true,
+          antispam: true,
+          antitroli: false,
+          backup: true,
+          backupDB: 0,
+          groupOnly: false,
+          jadibot: false,
+          nsfw: true,
+          onsfw: true,
+          status: 0,
+        }
+      } catch (e) {
+        console.error(e)
+      }
+      if (opts['nyimak']) return
+      if (!m.fromMe && opts['self']) return
+      if (opts['pconly'] && m.chat.endsWith('g.us')) return
+      if (opts['gconly'] && !m.chat.endsWith('g.us')) return
+      if (opts['swonly'] && m.chat !== 'status@broadcast') return
+      if (typeof m.text !== 'string') m.text = ''
+      for (let name in global.plugins) {
+        let plugin = global.plugins[name]
+        if (!plugin) continue
+        if (plugin.disabled) continue
+        if (!plugin.all) continue
+        if (typeof plugin.all !== 'function') continue
+        try {
+          await plugin.all.call(this, m, chatUpdate)
+        } catch (e) {
+          if (typeof e === 'string') continue
+          console.error(e)
+        }
+      }
+      if (m.isBaileys) return
+      if (m.isBaileys) return
+      if (m.chat.endsWith('broadcast')) return // Supaya tidak merespon di status
+      let blockList = conn.blocklist.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
+      if (blockList.includes(m.sender)) return // Pengguna yang diblokir tidak bisa menggunakan bot
+      m.exp += Math.ceil(Math.random() * 10)
+
+      let usedPrefix
+      let _user = global.db.data && global.db.data.users && global.db.data.users[m.sender]
+
+      let isROwner = [global.conn.user.jid, ...global.owner].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
+      let isOwner = isROwner || m.fromMe
+      let isMods = isOwner || global.mods.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
+      let isPrems = isROwner || db.data.users[m.sender].premium
+      let groupMetadata = m.isGroup ? this.chats.get(m.chat).metadata || await this.groupMetadata(m.chat) : {} || {}
+      let participants = m.isGroup ? groupMetadata.participants : [] || []
+      let user = m.isGroup ? participants.find(u => u.jid == m.sender) : {} // User Data
+      let bot = m.isGroup ? participants.find(u => u.jid == this.user.jid) : {} // Your Data
+      let isAdmin = user.isAdmin || user.isSuperAdmin || false // Is User Admin?
+      let isBotAdmin = bot.isAdmin || bot.isSuperAdmin || false // Are you Admin?
+      let isBlocked = this.blocklist.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != this.user.jid).includes(m.sender) // Apakah user diblokir?
+      let DevMode = (global.DeveloperMode.toLowerCase() == 'true') || false
+      for (let name in global.plugins) {
+        let plugin = global.plugins[name]
+        if (!plugin) continue
+        if (plugin.disabled) continue
+        //if (!opts['restrict']) if (plugin.tags && plugin.tags.includes('admin')) continue
+        // if (!opts['nsfw']) if (plugin.tags && plugin.tags.includes('hentai', 'bokep')) continue
+        const str2Regex = str => str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&')
+        let _prefix = plugin.customPrefix ? plugin.customPrefix : conn.prefix ? conn.prefix : global.prefix
+        let match = (_prefix instanceof RegExp ? // RegExp Mode?
+          [[_prefix.exec(m.text), _prefix]] :
+          Array.isArray(_prefix) ? // Array?
+            _prefix.map(p => {
+              let re = p instanceof RegExp ? // RegExp in Array?
+                p :
+                new RegExp(str2Regex(p))
+              return [re.exec(m.text), re]
+            }) :
+            typeof _prefix === 'string' ? // String?
+              [[new RegExp(str2Regex(_prefix)).exec(m.text), new RegExp(str2Regex(_prefix))]] :
+              [[[], new RegExp]]
+        ).find(p => p[1])
+        if (typeof plugin.before === 'function') if (await plugin.before.call(this, m, {
+          match,
+          conn: this,
+          participants,
+          groupMetadata,
+          user,
+          bot,
+          isROwner,
+          isOwner,
+          isAdmin,
+          isBotAdmin,
+          isPrems,
+          chatUpdate,
+          isBlocked,
+        })) continue
+        if (typeof plugin !== 'function') continue
+        if ((usedPrefix = (match[0] || '')[0])) {
+          let noPrefix = m.text.replace(usedPrefix, '')
+          let [command, ...args] = noPrefix.trim().split` `.filter(v => v)
+          args = args || []
+          let _args = noPrefix.trim().split` `.slice(1)
+          let text = _args.join` `
+          command = (command || '').toLowerCase()
+          let fail = plugin.fail || global.dfail // When failed
+          let isAccept = plugin.command instanceof RegExp ? // RegExp Mode?
+            plugin.command.test(command) :
+            Array.isArray(plugin.command) ? // Array?
+              plugin.command.some(cmd => cmd instanceof RegExp ? // RegExp in Array?
+                cmd.test(command) :
+                cmd === command
+              ) :
+              typeof plugin.command === 'string' ? // String?
+                plugin.command === command :
+                false
+
+          if (!isAccept) continue
+          m.plugin = name
+          if (m.chat in global.db.data.chats || m.sender in global.db.data.users) {
+            let chat = global.db.data.chats[m.chat]
+            let user = global.db.data.users[m.sender]
+            if (name != 'unbanchat.js' && chat && chat.isBanned) return // Except this
+            if (name != 'unbanuser.js' && user && user.banned) return
+          }
+          if (plugin.rowner && plugin.owner && !(isROwner || isOwner)) { // Both Owner
+            fail('owner', m, this)
+            continue
+          }
+          if (plugin.rowner && !isROwner) { // Real Owner
+            fail('rowner', m, this)
+            continue
+          }
+          if (plugin.owner && !isOwner) { // Number Owner
+            fail('owner', m, this)
+            continue
+          }
+          if (plugin.mods && !isMods) { // Moderator
+            fail('mods', m, this)
+            continue
+          }
+          if (plugin.premium && !isPrems) { // Premium
+            fail('premium', m, this)
+            continue
+          }
+          if (plugin.group && !m.isGroup) { // Group Only
+            fail('group', m, this)
+            continue
+          } else if (plugin.botAdmin && !isBotAdmin) { // You Admin
+            fail('botAdmin', m, this)
+            continue
+          } else if (plugin.admin && !isAdmin) { // User Admin
+            fail('admin', m, this)
+            continue
+          }
+          if (plugin.private && m.isGroup) { // Private Chat Only
+            fail('private', m, this)
+            continue
+          }
+          if (plugin.register == true && _user.registered == false) { // Butuh daftar?
+            fail('unreg', m, this)
+            continue
+          }
+
+          m.isCommand = true
+          let xp = 'exp' in plugin ? parseInt(plugin.exp) : 17 // XP Earning per command
+          if (xp > 200) m.reply('Ngecit -_-') // Hehehe
+          else m.exp += xp
+          if (!isPrems && plugin.limit && global.db.data.users[m.sender].limit < plugin.limit * 1) {
+            this.reply(m.chat, `✖️ Limit anda habis, silahkan beli melalui *${usedPrefix}blimit*`, m)
+            continue // Limit habis
+          }
+          if (plugin.level > _user.level) {
+            this.reply(m.chat, `[❗] Diperlukan level ${plugin.level} untuk menggunakan perintah ini\n📊 *Level kamu:* ${_user.level}`, m)
+            continue // If the level has not been reached
+          }
+          let extra = {
+            match,
+            usedPrefix,
+            noPrefix,
+            _args,
+            args,
+            command,
+            text,
+            conn: this,
+            participants,
+            groupMetadata,
+            user,
+            bot,
+            isROwner,
+            isOwner,
+            isAdmin,
+            isBotAdmin,
+            isPrems,
+            chatUpdate,
+            isBlocked,
+          }
+          try {
+            await plugin.call(this, m, extra)
+            if (!isPrems) m.limit = m.limit || plugin.limit || false
+          } catch (e) {
+            // Error occured
+            m.error = e
+            console.error(e)
+            if (e) {
+              let text = util.format(e.message ? e.message : e)
+              for (let key of Object.values(global.APIKeys))
+                text = text.replace(new RegExp(key, 'g'), '#HIDDEN#')
+              m.reply(text)
+            }
+          } finally {
+            // m.reply(util.format(_user))
+            if (typeof plugin.after === 'function') {
+              try {
+                await plugin.after.call(this, m, extra)
+              } catch (e) {
+                console.error(e)
+              }
+            }
+            if (m.limit) m.reply(+ m.limit + ' Limit terpakai ✔️')
+          }
+          break
+        }
+      }
+    } finally {
+      //console.log(global.db.data.users[m.sender])
+      let user, stats = global.db.data.stats
+      if (m) {
+        if (m.sender && (user = global.db.data.users[m.sender])) {
+          user.exp += m.exp
+          user.limit -= m.limit * 1
+        }
+
+        let stat
+        if (m.plugin) {
+          let now = + new Date
+          if (m.plugin in stats) {
+            stat = stats[m.plugin]
+            if (!isNumber(stat.total)) stat.total = 1
+            if (!isNumber(stat.success)) stat.success = m.error != null ? 0 : 1
+            if (!isNumber(stat.last)) stat.last = now
+            if (!isNumber(stat.lastSuccess)) stat.lastSuccess = m.error != null ? 0 : now
+          } else stat = stats[m.plugin] = {
+            total: 1,
+            success: m.error != null ? 0 : 1,
+            last: now,
+            lastSuccess: m.error != null ? 0 : now
+          }
+          stat.total += 1
+          stat.last = now
+          if (m.error == null) {
+            stat.success += 1
+            stat.lastSuccess = now
+          }
+        }
+      }
+
+      try {
+        require('./lib/print')(m, this)
+      } catch (e) {
+        console.log(m, m.quoted, e)
+      }
+    if (opts['autoread']) await this.chatRead(m.chat).catch(() => { })
+    this.chatRead(m.chat).catch(() => { })
+    }
+  },
+  async participantsUpdate({ jid, participants, action }) {
+    let chat = global.db.data.chats[jid] || {}
+    let text = ''
+    switch (action) {
+      case 'add':
+      case 'remove':
+        if (chat.welcome) {
+          let groupMetadata = await this.groupMetadata(jid)
+          for (let user of participants) {
+            //let pp = './src/avatar_contact.png'
+            try {
+              pp = await this.getProfilePicture(user)
+            } catch (e) {
+            } finally {
+              text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Welcome, @user!').replace('@subject', this.getName(jid)).replace('@desc', groupMetadata.desc) :
+                (chat.sBye || this.bye || conn.bye || 'Bye, @user!')).replace('@user', '@' + user.split('@')[0])
+              let wm = global.botwm
+              let wel = await (await fetch(fla + `WELCOME`)).buffer()
+
+              let lea = await (await fetch(fla + `GOOD BYE`)).buffer()
+
+await conn.sendMessage(jid, { "contentText": action === 'add' ? '─────[ *WELCOME* ]─────' : '──────[ *GOOD BYE* ]──────' , "footerText": text,
+"buttons": [
+{buttonId: '.menu', buttonText: {displayText: '⋮☰ Menu'}, type: 1},
+{buttonId: '.infogc', buttonText: {displayText: 'Info Group'}, type: 1},
+{buttonId: '.rules', buttonText: {displayText: 'Rules Bot'}, type: 1}
+],
+"headerType": "DOCUMENT", "documentMessage": {
+            "url": "https://mmg.whatsapp.net/d/f/AsO5KpESy9E0WI72xEVp65rx505bQxvuIma79L8Ue076.enc",
+            "mimetype": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+            "title": "ness.docx",
+            "fileSha256": "8Xfe3NQDhjwVjR54tkkShLDGrIFKR9QT5EsthPyxDCI=",
+            "fileLength": "99999999999999",
+            "pageCount": 100,
+            "mediaKey": "XWv4hcnpGY51qEVSO9+e+q6LYqPR3DbtT4iqS9yKhkI=",
+            "fileName": 'Creαted by : нєrмαทsyαн',
+            "fileEncSha256": "NI9ykWUcXKquea4BmH7GgzhMb3pAeqqwE+MTFbH/Wk8=",
+            "directPath": "/v/t62.7118-24/35150115_287008086621545_8250021012380583765_n.enc?ccb=11-4&oh=6f0f730e5224c054969c276a6276a920&oe=61A21F46",
+            "mediaKeyTimestamp": "1634472176",
+            "jpegThumbnail": await (await fetch('https://telegra.ph/file/6e45318d7c76f57e4a8bd.jpg')).buffer(),
+  }}, 'buttonsMessage', { quoted: false, contextInfo: { mentionedJid: [user], forwardingScore: 999, isForwarded: true, externalAdReply: { title: global.wm, body: action === 'add' ? 'Selamat Datang Kak!' : 'Yahh.. kok keluar', description: action === 'add' ? 'Selamat Datang Kak!' : 'Yahh.. kok keluar :‹', mediaType: 2, thumbnail: action === 'add' ? wel : lea, mediaUrl: `https://youtube.com/watch?v=uIedYGN3NQQ`}}})
+              
+            }
+          }
+        }
+        break
+      case 'promote':
+        text = (chat.sPromote || this.spromote || conn.spromote || '@user ```is now Admin```')
+      case 'demote':
+        if (!text) text = (chat.sDemote || this.sdemote || conn.sdemote || '@user ```is no longer Admin```')
+        text = text.replace('@user', '@' + participants[0].split('@')[0])
+        let banp = fs.readFileSync('./src/kanna.jpg')
+        if (chat.detect) this.send2ButtonLoc(jid, banp, text, '© 𖡞ђ૯૨ʍ᭄αท вѳτż', 'MENU', '#menu', 'OWNER', '#owner', null, {
+          contextInfo: {
+            mentionedJid: this.parseMention(text)
+          }
+        })
+        break
+    }
+  },
+  async delete(m) {
+    if (m.key.fromMe) return
+    let chat = global.db.data.chats[m.key.remoteJid]
+    if (chat.delete) {
+    await this.sendButton(m.key.remoteJid, `*—「 Anti Delete 」—*
+*📢 Terdeteksi Penghapusan Pesan !*
+*◇ Nama :* @${m.participant.split`@`[0]}
+*◇ Type*: ${Object.keys(m.message.message)[0]}
+*◇ Number*: ${require('awesome-phonenumber')(`+${m.participant.split`@`[0]}`).getNumber('international')}
+
+klick untuk mematikannya atau ketik #disable delete
+`.trim(), '© 𖡞ђ૯૨ʍ᭄αท вѳτż', 'DISABLE DELETE', '.disable delete', {
+      quoted: m.message,
+      contextInfo: {
+        mentionedJid: [m.participant]
+      }
+    })
+    this.copyNForward(m.key.remoteJid, m.message).catch(e => console.log(e, m))
+  }
+},
+  async onCall(json) {
+    let { from } = json[2][0][1]
+    let users = global.db.data.users
+    let user = users[from] || {}
+    if (user.whitelist) return
+    if (!db.data.settings.anticall) return
+    switch (this.callWhitelistMode) {
+      case 'mycontact':
+        if (from in this.contacts && 'short' in this.contacts[from])
+          return
+        break
+    }
+    await this.sendMessage(from, '[❗] karena anda menelfon bot. anda diblokir otomatis', MessageType.extendedText)
+    await this.blockUser(from, 'add')
+  }
+}
+
+global.dfail = (type, m, conn) => {
+
+let wm = global.botwm
+	let botdate = global.botdate
+	let syappa = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+	let namae = conn.getName(syappa)
+let usr = db.data.users[m.sender]
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let user = conn.getName(who)
+  let msg = {
+    rowner: '[❗] Creator Only',
+    owner: '[❗] Owner Only',
+    mods: '[❗] Moderator Only',
+    premium: '[❗] Premium Only',
+    group: '[❗] Group Only',
+    private: '[❗] Private Only',
+    admin: '[❗] Admin Group Only',
+    nsfw: '[❗] Nsfw Not Active',
+    botAdmin: '[❗] Bot Admin Only'
+    /*unreg: `*── 「 NOT REGISTERED 」 ──*
+Halo @${m.sender.split`@`[0]} !
+Yuk Daftar Dulu Karena Anda Belum Terdaftar Dalam Database Bot
+    
+📍 Ketik : #daftar nama.umur
+▸ Contoh : #daftar ${user}.13`*/
+  }[type]
+  if (msg) return m.reply(msg)
+  
+  let msgg = {
+  unreg: `*── 「 NOT REGISTERED 」 ──*
+Halo kak @${syappa.replace(/@.+/, '')} !
+Yuk Daftar Dulu Karena Anda Belum Terdaftar Dalam Database Bot 🗂️
+    
+📍 *Ketik :* #daftar nama.umur
+⤿ *Contoh :* #daftar ${namae}.19`
+}[type]
+  if (msgg) return conn.sendButton(m.chat, msgg, botdate + '\n' + global.wm, 'Verify', `.daftar ${namae}.18`, m, { contextInfo: { mentionedJid: [syappa] }})
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'handler.js'"))
+  delete require.cache[file]
+  if (global.reloadHandler) console.log(global.reloadHandler())
+})
