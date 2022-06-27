dbbca92aa527fdabe81e479b228d80b2edb4736c
Dimzga <106530321+Dimzga@users.noreply.github.com>
2022-06-27 17:05:15 +0700

    Update judi.js

--- plugins/judi.js
+++ plugins/judi.js
@@ -1,139 +1,61 @@
-let { MessageType } = require('@adiwajshing/baileys')
-
-let wm = global.botwm
-let confirm = {}
-
-async function handler(m, { conn, args, isROwner }) {
-	if (!db.data.chats[m.chat].rpg && m.isGroup) throw 'Feature Rpg Dimatikan Di grup ini\nKetik *!on* *rpg* untuk mengaktifkan fitur'
-    //if (!isROwner) throw 'Dalam perbaikan'
-    if (m.sender in confirm) throw 'Kamu masih melakukan judi, tunggu sampai selesai!!'
+let buatall = 1
+let handler = async (m, { conn, args, usedPrefix, isOwner }) => {
+    conn.judi = conn.judi ? conn.judi : {}
+    if (m.chat in conn.judi) return m.reply ('Masih ada yang melakukan judi disini, tunggu sampai selesai!!')
+    else conn.judi[m.chat] = true
     try {
-        let user = global.db.data.users[m.sender]
-        let count = (args[0] && number(parseInt(args[0])) ? Math.max(parseInt(args[0]), 1) : /all/i.test(args[0]) ? Math.floor(parseInt(user.money)) : 1) * 1
-        if ((user.money * 1) < count) return conn.sendMessage(m.chat, button('💵Uang kamu tidak cukup!!', user), MessageType.buttonsMessage, { quoted: m })
-        if (!(m.sender in confirm)) {
-            confirm[m.sender] = {
-                sender: m.sender,
-                count,
-                timeout: setTimeout(() => (m.reply('timed out'), delete confirm[m.sender]), 60000)
-            }
-            let txt = '*❗ Warning ❗*\nJangan judi karena tidak akan menang, BENERAN!!\nApakah anda yakin  mau melakukan judi?\n⏲️ *Timeout* : 60s'
-            const buttons = [
-                {buttonId: `id1`, buttonText: {displayText: '✔️'}, type: 1},
-                {buttonId: `id2`, buttonText: {displayText: '✖️'}, type: 1}
-            ]
-
-            const buttonMessage = {
-                contentText: txt,
-                footerText: `${wm}`,
-                buttons: buttons,
-                headerType: 1
-            }
-            return conn.sendMessage(m.chat, buttonMessage, MessageType.buttonsMessage, { quoted: m })
-        }
-    } catch (e) {
-        if (m.sender in confirm) {
-            let { timeout } = confirm[m.sender]
-            clearTimeout(timeout)
-            delete confirm[m.sender]
-            m.reply('Rejected')
-        }
-    }
-}
-
-handler.before = async m => {
-    if (!(m.sender in confirm)) return 
-    if (m.isBaileys) return 
-    let { timeout, count } = confirm[m.sender]
-    let user = global.db.data.users[m.sender]
-    let moneyDulu = user.money * 1
-    let txt = (m.msg && m.msg.selectedDisplayText ? m.msg.selectedDisplayText : m.text ? m.text : '').toLowerCase()
-    try {
-        if (/^(yes|ya|✔️)?$/i.test(txt)) {
-            let Bot = (Math.ceil(Math.random() * 1)) * 1
-            let Kamu = (Math.floor(Math.random() * 86)) * 1
-            let status = 'Kalah'
-            if (Bot < Kamu) {
-                user.money += count * 1
-                status = 'Menang'
-            } else if (Bot > Kamu) {
-                user.money -= count * 1
+    	let __waktutionskh = (new Date - global.db.data.users[m.sender].judilast)
+        let _waktutionskh = (5000 - __waktutionskh)
+        let waktutionskh = clockString(_waktutionskh)
+        if (new Date - global.db.data.users[m.sender].judilast > 5000) {
+        global.db.data.users[m.sender].judilast = new Date * 1
+        let randomaku = `${Math.floor(Math.random() * 101)}`.trim()
+        let randomkamu = `${Math.floor(Math.random() * 75)}`.trim()                //hehe Biar Susah Menang :v
+        let Aku = (randomaku * 1)
+        let Kamu = (randomkamu * 1)
+        let count = args[0]
+        count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].money / buatall) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
+        count = Math.max(1, count)
+        if (args.length < 1) return conn.reply(m.chat, usedPrefix + 'judi <jumlah>\n ' + usedPrefix + 'judi 1000', m)
+        if (global.db.data.users[m.sender].money >= count * 1) {
+            global.db.data.users[m.sender].money -= count * 1
+            await m.reply('*Jangan judi gk bakal menang!!, kalau gk percaya gpp*') //Kwkwwkkwlwlw
+            if (Aku > Kamu) {
+                conn.reply(m.chat, `aku roll:${Aku}\nKamu roll: ${Kamu}\n\nkamu *Kalah*, kamu kehilangan ${count} money`.trim(), m)
+            } else if (Aku < Kamu) {
+                global.db.data.users[m.sender].money += count * 2
+                conn.reply(m.chat, `aku roll:${Aku}\nKamu roll: ${Kamu}\n\nkamu *Menang*, kamu Mendapatkan ${count * 2} money`.trim(), m)
             } else {
-                status = 'Seri'
-                user.money += (Math.floor(count / 1.5)) * 1
+                global.db.data.users[m.sender].money += count * 1
+                conn.reply(m.chat, `aku roll:${Aku}\nKamu roll: ${Kamu}\n\nkamu *Seri*, kamu Mendapatkan ${count * 1} money`.trim(), m)
             }
-            m.reply(`
-🤖 *Bot roll:* ${Bot}
-👤 *Kamu roll:* ${Kamu}
-
-▸ Kamu *${status}*, kamu ${status == 'Menang' ? `Mendapatkan *+${count * 2}*` : status == 'Kalah' ? `Kehilangan *-${count * 1}*` : `Mendapatkan *+${Math.floor(count / 1.5)}*`} Money 💵
-    `.trim())
-            clearTimeout(timeout)
-            delete confirm[m.sender]
-            return !0
-        } else if (/^(no|tidak|ga|✖️)?$/i.test(txt)) {
-            clearTimeout(timeout)
-            delete confirm[m.sender]
-            m.reply('Rejected')
-            return !0
-        }
-
+        } else conn.reply(m.chat, `Money kamu tidak cukup untuk melakukan judi sebesar ${count} money`.trim(), m)
+      } else conn.reply(m.chat, `Kamu sudah judi, tidak bisa judi kembali..\nMohon tunggu ${waktutionskh} lagi untuk judi kembali `, m)
     } catch (e) {
-        clearTimeout(timeout)
-        delete confirm[m.sender]
-        if (moneyDulu > (user.money * 1)) user.money = moneyDulu * 1
-        m.reply('Error saat melakukan judi (Rejected)')
-        return !0
-    } finally {
-        clearTimeout(timeout)
-        delete confirm[m.sender]
-        return !0
+        console.log(e)
+        m.reply('Error!!')
+   } finally {
+        delete conn.judi[m.chat]
     }
-}
-  
-handler.help = ['judi [jumlah]']
-handler.tags = ['rpg']
+ }
+handler.help = ['judi <jumlah>']
+handler.tags = ['game']
 handler.command = /^(judi)$/i
+handler.limit = true
+handler.group = true
+
+handler.fail = null
 
 module.exports = handler
 
-/**
- * Detect if thats number
- * @param {Number} x 
- * @returns Boolean
- */
-function number(x = 0) {
-    x = parseInt(x)
-    return !isNaN(x) && typeof x == 'number'
+function pickRandom(list) {
+    return list[Math.floor(Math.random() * list.length)]
 }
 
-function button(teks, user) {
-    let buttons = []
-    
-    let claim = new Date - user.lastclaim > 86400000
-    let monthly = new Date - user.lastmonthly > 2592000000
-    let weekly = new Date - user.lastweekly > 604800000
-    let adventure = new Date - user.lastadventure > 300000
-    let fishing = new Date - user.lastfishing > 300000
-    let mining = new Date - user.lastmining > 300000
-    let dungeon = new Date - user.lastdungeon > 600000
-    console.log({claim, monthly, weekly, adventure, fishing, mining, dungeon})
-
-    if (monthly) buttons.push({buttonId: `id${buttons.length + 1}`, buttonText: {displayText: '/monthly'}, type: 1})
-    if (weekly) buttons.push({buttonId: `id${buttons.length + 1}`, buttonText: {displayText: '/weekly'}, type: 1})
-    if (claim) buttons.push({buttonId: `id${buttons.length + 1}`, buttonText: {displayText: '/claim'}, type: 1})
-    if (adventure) buttons.push({buttonId: `id${buttons.length + 1}`, buttonText: {displayText: '/adventure'}, type: 1})
-    if (fishing) buttons.push({buttonId: `id${buttons.length + 1}`, buttonText: {displayText: '/fishing'}, type: 1})
-    if (mining) buttons.push({buttonId: `id${buttons.length + 1}`, buttonText: {displayText: '/mining'}, type: 1})
-    if (dungeon) buttons.push({buttonId: `id${buttons.length + 1}`, buttonText: {displayText: '/dungeon'}, type: 1})
-    if (buttons.length == 0) throw teks
-    
-    const buttonMessage = {
-        contentText: teks,
-        footerText: `${wm}`,
-        buttons: buttons,
-        headerType: 1
-    }
-    
-    return buttonMessage
+function clockString(ms) {
+  let h = Math.floor(ms / 3600000)
+  let m = Math.floor(ms / 60000) % 60
+  let s = Math.floor(ms / 1000) % 60
+  console.log({ms,h,m,s})
+  return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
 }
