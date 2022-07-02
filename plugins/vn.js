d28f2172b75d0c715538f03d17dc25dbe3fdbfcf
Dimzga <106530321+Dimzga@users.noreply.github.com>
2022-07-02 23:56:39 +0700

    Update vn.js

--- plugins/vn.js
+++ plugins/vn.js
@@ -8,7 +8,7 @@ json = await res.buffer()
 
 handler.help = ['anjay', 'ara-ara', 'ara-ara-cowok', 'ara-ara2', 'arigatou', 'assalamualaikum', 'asu', 'ayank', 'aku-ngakak', 'bacot', 'bahagia-aku', 'baka', 'bansos', 'beat-box', 'beat-box2', 'biasalah', 'bidadari', 'bot', 'buka-pintu', 'canda-anjing', 'cepetan', 'china', 'cuekin-terus', 'daisuki-dayo', 'daisuki', 'dengan-mu', 'gaboleh-gitu', 'gak-lucu', 'gamau', 'gay', 'gelay', 'gitar', 'gomenasai', 'hai-bot', 'hampa', 'hayo', 'hp-iphone', 'i-like-you', 'ih-wibu', 'india', 'karna-lo-wibu', 'kiss','kontol', 'ku-coba', 'maju-wibu', 'makasih', 'mastah', 'menu', 'nande-nande', 'nani', 'ngadi-ngadi', 'nikah', 'nuina', 'onichan', 'owner-sange', 'ownerku', 'pak-sapardi', 'pale', 'pantek', 'pasi-pasi', 'punten', 'sayang', 'siapa-sih', 'sudah-biasa', 'summertime', 'tanya-bapak-lu', 'to-the-bone', 'wajib', 'waku', 'woi', 'yamete', 'yowaimo', 'yoyowaimo']
 handler.tags = ['vn']
-handler.costumPrefix = /(anjay|ara-ara|ara-ara-cowok|ara-ara2|arigatou|assalamualaikum|asu|ayank|aku-ngakak|bacot|bahagia-aku|baka|bansos|beat-box|beat-box2|biasalah|bidadari|bot|buka-pintu|canda-anjing|cepetan|china|cuekin-terus|daisuki-dayo|daisuki|dengan-mu|gaboleh-gitu|gak-lucu|gamau|gay|gelay|gitar|gomenasai|hai-bot|hampa|hayo|hp-iphone|i-like-you|ih-wibu|india|karna-lo-wibu|kiss|kontol|ku-coba|maju-wibu|makasih|mastah|menu|nande-nande|nani|ngadi-ngadi|nikah|nuina|onichan|owner-sange|ownerku|pak-sapardi|pale|pantek|pasi-pasi|punten|sayang|siapa-sih|sudah-biasa|summertime|tanya-bapak-lu|to-the-bone|wajib|waku|woi|yamete|yowaimo|yoyowaimo)$/i
+handler.costumPrefix = /^(anjay|ara-ara|ara-ara-cowok|ara-ara2|arigatou|assalamualaikum|asu|ayank|aku-ngakak|bacot|bahagia-aku|baka|bansos|beat-box|beat-box2|biasalah|bidadari|bot|buka-pintu|canda-anjing|cepetan|china|cuekin-terus|daisuki-dayo|daisuki|dengan-mu|gaboleh-gitu|gak-lucu|gamau|gay|gelay|gitar|gomenasai|hai-bot|hampa|hayo|hp-iphone|i-like-you|ih-wibu|india|karna-lo-wibu|kiss|kontol|ku-coba|maju-wibu|makasih|mastah|menu|nande-nande|nani|ngadi-ngadi|nikah|nuina|onichan|owner-sange|ownerku|pak-sapardi|pale|pantek|pasi-pasi|punten|sayang|siapa-sih|sudah-biasa|summertime|tanya-bapak-lu|to-the-bone|wajib|waku|woi|yamete|yowaimo|yoyowaimo)$/i
 handler.command = new RegExp
 handler.owner = false
 handler.mods = false
@@ -23,5 +23,3 @@ handler.botAdmin = false
 handler.fail = null
 
 module.exports = handler
-
-
