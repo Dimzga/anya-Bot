d57fdef7c5423c29d786a4e33c26c3ad4eca729b
Dimzga <106530321+Dimzga@users.noreply.github.com>
2022-07-02 20:21:14 +0700

    Update lolice.js

--- plugins/lolice.js
+++ plugins/lolice.js
@@ -2,8 +2,8 @@ let handler = async(m,{text, conn}) => {
 let supa = 'https://api.lolhuman.xyz/api/sticker/gawrgura?apikey=7b389b5e346e7b9181783e80'
 conn.sendFile(m.chat, supa, null, 'Nih', m)
 }
-handler.help = ['anime']
+handler.help = ['gura']
 handler.tags = ['anime']
-handler.command = /^(gawrgura)$/i
+handler.command = /^(gura)$/i
 
 module.exports = handler
