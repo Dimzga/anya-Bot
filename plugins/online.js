a0a66ae1b06e981613f9e2100df17c501b665190
Dimzga <106530321+Dimzga@users.noreply.github.com>
2022-07-06 12:57:43 +0700

    Update online.js

--- plugins/online.js
+++ plugins/online.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, args }) => {
   let id = args && /\d+\-\d+@g.us/.test(args[0]) ? args[0] : m.chat
-  let online = [...Object.keys(conn.chats.get(id).presences), conn.user.jid]
+  let online = [...Object.keys(store.presences[id]), conn.user.jid]
   conn.reply(m.chat, 'List Online:\n' + online.map(v => '- @' + v.replace(/@.+/, '')).join`\n`, m, {
     contextInfo: { mentionedJid: online }
   })
