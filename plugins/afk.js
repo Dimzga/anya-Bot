901900daeab327e93a3fb78c45cc6f2026d6c9ec
Dimzga <106530321+Dimzga@users.noreply.github.com>
2022-07-08 00:06:10 +0700

    Update afk.js

--- plugins/afk.js
+++ plugins/afk.js
@@ -1,7 +1,6 @@
 let handler = async(m, { conn, usedPrefix, text }) => {
     let user = global.db.data.users[m.sender]
-let bg = await (await fetch('https://telegra.ph/file/46a2634596ce1a9e78d8c.mp4')).buffer()
-    await conn.send3ButtonVid(m.chat, bg, text.trim(),"
+const bg = "https://telegra.ph/file/e5847d1c9df8caa6bc6b7.mp4"
     user.afk = + new Date
     user.afkReason = text
     conn.sendButton(m.chat, `
