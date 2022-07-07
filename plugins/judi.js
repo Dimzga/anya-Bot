6a3825be84ed937bb2c9dbff8912f09879738207
Dimzga <106530321+Dimzga@users.noreply.github.com>
2022-07-07 08:44:26 +0700

    Update judi.js

--- plugins/judi.js
+++ plugins/judi.js
@@ -1,61 +1,93 @@
-let buatall = 1
-let handler = async (m, { conn, args, usedPrefix, isOwner }) => {
-    conn.judi = conn.judi ? conn.judi : {}
-    if (m.chat in conn.judi) return m.reply ('Masih ada yang melakukan judi disini, tunggu sampai selesai!!')
-    else conn.judi[m.chat] = true
+let confirm = {}
+
+async function handler(m, { conn, args }) {
+    //if (!isROwner) throw 'Dalam perbaikan'
+    if (m.sender in confirm) throw 'Kamu masih melakukan judi, tunggu sampai selesai!!'
     try {
-    	let __waktutionskh = (new Date - global.db.data.users[m.sender].judilast)
-        let _waktutionskh = (5000 - __waktutionskh)
-        let waktutionskh = clockString(_waktutionskh)
-        if (new Date - global.db.data.users[m.sender].judilast > 5000) {
-        global.db.data.users[m.sender].judilast = new Date * 1
-        let randomaku = `${Math.floor(Math.random() * 101)}`.trim()
-        let randomkamu = `${Math.floor(Math.random() * 75)}`.trim()                //hehe Biar Susah Menang :v
-        let Aku = (randomaku * 1)
-        let Kamu = (randomkamu * 1)
-        let count = args[0]
-        count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].money / buatall) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
-        count = Math.max(1, count)
-        if (args.length < 1) return conn.reply(m.chat, usedPrefix + 'judi <jumlah>\n ' + usedPrefix + 'judi 1000', m)
-        if (global.db.data.users[m.sender].money >= count * 1) {
-            global.db.data.users[m.sender].money -= count * 1
-            await m.reply('*Jangan judi gk bakal menang!!, kalau gk percaya gpp*') //Kwkwwkkwlwlw
-            if (Aku > Kamu) {
-                conn.reply(m.chat, `aku roll:${Aku}\nKamu roll: ${Kamu}\n\nkamu *Kalah*, kamu kehilangan ${count} money`.trim(), m)
-            } else if (Aku < Kamu) {
-                global.db.data.users[m.sender].money += count * 2
-                conn.reply(m.chat, `aku roll:${Aku}\nKamu roll: ${Kamu}\n\nkamu *Menang*, kamu Mendapatkan ${count * 2} money`.trim(), m)
-            } else {
-                global.db.data.users[m.sender].money += count * 1
-                conn.reply(m.chat, `aku roll:${Aku}\nKamu roll: ${Kamu}\n\nkamu *Seri*, kamu Mendapatkan ${count * 1} money`.trim(), m)
+        let user = global.db.data.users[m.sender]
+        let count = (args[0] && number(parseInt(args[0])) ? Math.max(parseInt(args[0]), 1) : /all/i.test(args[0]) ? Math.floor(parseInt(user.money)) : 1) * 1
+        if ((user.money * 1) < count) return m.reply('💹 Uang kamu tidak cukup!!')
+        if (!(m.sender in confirm)) {
+            confirm[m.sender] = {
+                sender: m.sender,
+                count,
+                timeout: setTimeout(() => (m.reply('timed out'), delete confirm[m.sender]), 60000)
             }
-        } else conn.reply(m.chat, `Money kamu tidak cukup untuk melakukan judi sebesar ${count} money`.trim(), m)
-      } else conn.reply(m.chat, `Kamu sudah judi, tidak bisa judi kembali..\nMohon tunggu ${waktutionskh} lagi untuk judi kembali `, m)
+            let txt = `Apakah anda yakin mau melakukan judi (Y/n)\n\n*Taruhan:* ${count} 💹\n⏰ 60s Timeout`
+            return conn.sendButton(m.chat, txt, author, null, [['✔️'], ['✖️']], m)
+        }
     } catch (e) {
-        console.log(e)
-        m.reply('Error!!')
-   } finally {
-        delete conn.judi[m.chat]
+        console.error(e)
+        if (m.sender in confirm) {
+            let { timeout } = confirm[m.sender]
+            clearTimeout(timeout)
+            delete confirm[m.sender]
+            m.reply('Rejected')
+        }
     }
- }
-handler.help = ['judi <jumlah>']
-handler.tags = ['game']
-handler.command = /^(judi)$/i
-handler.limit = true
-handler.group = true
-
-handler.fail = null
-
-module.exports = handler
-
-function pickRandom(list) {
-    return list[Math.floor(Math.random() * list.length)]
 }
 
-function clockString(ms) {
-  let h = Math.floor(ms / 3600000)
-  let m = Math.floor(ms / 60000) % 60
-  let s = Math.floor(ms / 1000) % 60
-  console.log({ms,h,m,s})
-  return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
+handler.before = async m => {
+    if (!(m.sender in confirm)) return
+    if (m.isBaileys) return
+    let { timeout, count } = confirm[m.sender]
+    let user = global.db.data.users[m.sender]
+    let moneyDulu = user.money * 1
+    let txt = (m.msg && m.msg.selectedDisplayText ? m.msg.selectedDisplayText : m.text ? m.text : '').toLowerCase()
+    try {
+        if (/^(✔️|y(es|a))?$/i.test(txt)) {
+            let Bot = (Math.ceil(Math.random() * 20)) * 1
+            let Kamu = (Math.floor(Math.random() * 80)) * 1
+            let status = 'Kalah'
+            if (Bot < Kamu) {
+                user.money += count * 1
+                status = 'Menang'
+            } else if (Bot > Kamu) {
+                user.money -= count * 1
+            } else {
+                status = 'Seri'
+                user.money += (Math.floor(count / 1.5)) * 1
+            }
+            m.reply(`
+| *PLAYERS* | *POINT* |
+*🤖 BOT:*      ${Bot}
+*👤 KAMU:*    ${Kamu}
+
+Kamu *${status}*, kamu ${status == 'Menang' ? `Mendapatkan *+${count * 2}*` : status == 'Kalah' ? `Kehilangan *-${count * 1}*` : `Mendapatkan *+${Math.floor(count / 1.5)}*`} Money 💹
+    `.trim())
+            clearTimeout(timeout)
+            delete confirm[m.sender]
+            return !0
+        } else if (/^(✖️|no)?$/i.test(txt)) {
+            clearTimeout(timeout)
+            delete confirm[m.sender]
+            m.reply('Rejected')
+            return !0
+        }
+
+    } catch (e) {
+        clearTimeout(timeout)
+        delete confirm[m.sender]
+        if (moneyDulu > (user.money * 1)) user.money = moneyDulu * 1
+        m.reply('Error saat melakukan judi (Rejected)')
+        return !0
+    } finally {
+        clearTimeout(timeout)
+        delete confirm[m.sender]
+        return !0
+    }
+}
+
+handler.help = ['judi [jumlah]']
+handler.tags = ['rpg']
+handler.command = /^(judi|bet)$/i
+
+/**
+ * Detect if thats number
+ * @param {Number} x 
+ * @returns Boolean
+ */
+function number(x = 0) {
+    x = parseInt(x)
+    return !isNaN(x) && typeof x == 'number'
 }
