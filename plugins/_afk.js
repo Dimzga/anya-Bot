5299d5ca7f053519ba91bf3d76c1b6b758d7493d
Dimzga <106530321+Dimzga@users.noreply.github.com>
2022-07-08 00:22:24 +0700

    Update _afk.js

--- plugins/_afk.js
+++ plugins/_afk.js
@@ -1,10 +1,9 @@
-let bg = await (await fetch('https://telegra.ph/file/46a2634596ce1a9e78d8c.mp4')).buffer()
-    await conn.send3ButtonVid(m.chat, bg, text.trim(),
+const bg = "https://telegra.ph/file/46a2634596ce1a9e78d8c.mp4"
 let handler = m => m
 handler.before = function (m) {
     let user = global.db.data.users[m.sender]
     if (user.afk > -1) {
-        this.sendButtonImg(m.chat, bg, `
+        this.sendButtonVid(m.chat, bg, `
 Kamu berhenti AFK${user.afkReason ? ' setelah ' + user.afkReason : ''}
 Selama ${this.clockString(new Date - user.afk)}
 `.trim(), wm, `Menu`, `.menu`, m)
@@ -18,7 +17,7 @@ Selama ${this.clockString(new Date - user.afk)}
         let afkTime = user.afk
         if (!afkTime || afkTime < 0) continue
         let reason = user.afkReason || ''
-        this.sendButtonImg(m.chat, bg, `
+        this.sendButtonVid(m.chat, bg, `
 Jangan tag dia YA SAYANG 💕!
 Dia sedang AFK ${reason ? 'dengan alasan ' + reason : 'tanpa alasan'}
 Selama ${this.clockString(new Date - afkTime)}
