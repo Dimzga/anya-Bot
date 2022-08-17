bbd1cdbf12fecc33c408b8806e0da05679aef7a0
Dimzga <106530321+Dimzga@users.noreply.github.com>
2022-08-17 18:30:25 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -2,7 +2,7 @@ let levelling = require('../lib/levelling')
 let fs = require('fs')
 let path = require('path')
 let fetch = require('node-fetch')
-let moment = require('moment-timezone')('Asia/Jakarta').format('HH')
+let moment = require('moment-timezone'),('Asia/Jakarta').format('HH')
 const defaultMenu = {
   before: `
  ❖❯────【SENA BOT】────❮❖
@@ -303,7 +303,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     let bg = await (await fetch('https://telegra.ph/file/62d5a39cced46b63fa7e0.mp4')).buffer()
-    await conn.send3ButtonVid(m.chat, bg, text.trim(), wm, `🏅Owner`, `${_p}owner`, `🎖ThanksTo`, `${_p}tqto`, `\n AKU PEDO DAN AKU BANGGA`, `${_p}infobot`)
+    await conn.send3ButtonVid(m.chat, bg, text.trim(), wm, `🏅Owner`, `${_p}owner`, `🎖ThanksTo`, `${_p}tqto`, `\n AKU PEDO DAN AKU BANGGA`, `${_p}rules`)
   } catch (e) {
     conn.reply(m.chat, 'Error by ganzzz', m)
     throw e
