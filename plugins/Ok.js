4793e9d42889079f3236e6d9f3adaeabd1e633bf
Dimzga <106530321+Dimzga@users.noreply.github.com>
2022-08-19 10:28:28 +0700

    Update Ok.js

--- plugins/Ok.js
+++ plugins/Ok.js
@@ -1,7 +1,7 @@
 const { sticker } = require('../lib/sticker')
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-let thumb = "https://telegra.ph/file/f61a7c96f3f86b6c5214f.png")).buffer()
+let thumb = "https://telegra.ph/file/f61a7c96f3f86b6c5214f.png"
 let stiker = await sticker(null, thumb, global.wm, global.author)
 await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, { asSticker: true })
 }
