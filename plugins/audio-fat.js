9ac630ed72f755ecbcda018db61dbe74038c677b
Dimzga <106530321+Dimzga@users.noreply.github.com>
2022-08-19 09:59:44 +0700

    Update audio-fat.js

--- plugins/audio-fat.js
+++ plugins/audio-fat.js
@@ -1,31 +1,19 @@
-const fs = require('fs')
-const { exec } = require('child_process')
-
-let handler = async (m, { conn, usedPrefix, command }) => {
-    try {
-        let q = m.quoted ? { message: { [m.quoted.mtype]: m.quoted } } : m
-        let mime = ((m.quoted ? m.quoted : m.msg).mimetype || '')
-        if (/audio/.test(mime)) {
-            let media = await conn.downloadAndSaveMediaMessage(q)
-            let ran = getRandom('.mp3')
-            exec(`ffmpeg -i ${media} -filter:a "atempo=1.6,asetrate=22100"  ${ran}`, (err, stderr, stdout) => {
-                fs.unlinkSync(media)
-                if (err) throw `_*Error!*_`
-                let buff = fs.readFileSync(ran)
-                conn.sendFile(m.chat, buff, ran, null, m, true, { quoted: m, mimetype: 'audio/mp4' })
-                fs.unlinkSync(ran)
-            })
-        } else throw `Balas vn/audio yang ingin diubah dengan caption *${usedPrefix + command}*`
-    } catch (e) {
-        throw e
-    }
+let handler = async (m, { conn, usedPrefix }) => {
+	await conn.fetchBlocklist().then(async data => {
+		let txt = `*「  Daftar Nomor Yang Diblacklist  」*\n\n*Total:* ${data.length}\n\n┌─\n`
+		for (let i of data) {
+			txt += `├ @${i.split("@")[0]}\n`
+		}
+		txt += "└────"
+		return m.reply(txt)
+	}).catch(err => {
+		console.log(err);
+		throw 'tidak ada yang diblokir!'
+	})
 }
-handler.help = ['fat']
-handler.tags = ['audio']
-handler.command = /^(fat)$/i
+
+handler.tags = ["info"]
+handler.help = ["listblock"]
+handler.command = /^(list(blo(ck|k)?)?)$/i
 
 module.exports = handler
-
-const getRandom = (ext) => {
-    return `${Math.floor(Math.random() * 10000)}${ext}`
-}
