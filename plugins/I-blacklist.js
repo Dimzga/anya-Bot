ffd0704d530c935dc6566a4a1f3e0fd7825b98ca
Dimzga <106530321+Dimzga@users.noreply.github.com>
2022-08-21 07:53:16 +0700

    Update I-blacklist.js

--- plugins/I-blacklist.js
+++ plugins/I-blacklist.js
@@ -1,4 +1,9 @@
 let handler = async (m, { conn, usedPrefix }) => {
+const bg = "https://telegra.ph/file/64295f11b9528f8caaf4b.mp4"
+
+
+conn.sendMessage(m.chat, {
+video: { url: bg },
 	await conn.fetchBlocklist().then(async data => {
 		let txt = `*「  Daftar Nomor Yang Diblacklist  」*\n\n*Total:* ${data.length}\n\n┌─\n`
 		for (let i of data) {
