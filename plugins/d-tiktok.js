5d76bd6295c88e5bb2ea1d4df9f3eb235bcb71a1
Dimzga <106530321+Dimzga@users.noreply.github.com>
2022-10-27 20:41:47 +0700

    Update d-tiktok.js

--- plugins/d-tiktok.js
+++ plugins/d-tiktok.js
@@ -1,4 +1,4 @@
-const { tiktokdl, tiktokdlv2, tiktokdlv3 } = require('@bochilteam/scraper')
+let axios = require('axios');
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
     let res = (await axios.get(API('males', '/tiktok', { url: args[0] } ))).data;
@@ -10,5 +10,6 @@ handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 handler.command = /^(tik(tok)?(dl)?)$/i
 
-module.exports = handler
+handler.limit = 1
 
+module.exports = handler
